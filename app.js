5712a965b56827219d84cee0fce3d354c4a809bc
Dragos Sarpe <contact@usdgadget.com>
2018-11-07 19:16:02 +0200

    Added new items to the UI

--- app.js
+++ app.js
@@ -76,7 +76,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputBtn: '.add__btn'
+		inputBtn: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: '.expenses__list'
 	};
 
 	return {
@@ -86,6 +88,34 @@ var UIController = (function() {
 				description: document.querySelector(DOMStrings.inputDescription).value,
 				value: document.querySelector(DOMStrings.inputValue).value
 			};
+		},
+		addListItem: function(obj, type) {
+			var html, element, newHtml;
+
+			// Create HTML string with placeholder text
+			if (type === 'inc') {
+				element = DOMStrings.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = DOMStrings.expensesContainer;
+				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			// Replace the placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+
+			// Insert the HTML into the DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+			/*
+			  Using the 'beforeend' keyword all the html code will be inserted 
+			  as a child of the 'element' variable that is '.income__list' or 
+			  '.expenses__list', but as a last child. So as the last element 
+			  in the list.
+			*/
+
 		},
 		getDOMStrings: function() {
 			return DOMStrings;
@@ -124,6 +154,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. Add the item to the UI
+		UICtrl.addListItem(newItem, input.type); 
 
 		// 4. Calculate the budget
 
