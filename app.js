74c412ad7df58201f0648791dca57f7c6f851169
Dragos Sarpe <contact@usdgadget.com>
2018-11-07 10:01:29 +0200

    Added event listeners

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
 	 
 })();
 
+
+
+// UI CONTROLLER
 var UIController = (function() {
 
 
 })();
 
+
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+	var ctrlAddItem = function() {
+		// 1. Get the field input data
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+
+		console.log('Function called!');
+	};
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+
+		// event.which is for older browsers
+
+		if (event.keyCode === 13 || event.which === 13) {     
+			ctrlAddItem();
+		}
+	});
 
 })(budgetController, UIController);
