61f1a1cb87aab246416f09f85c369b568aa14d41
Dragos Sarpe <contact@usdgadget.com>
2018-11-07 17:38:32 +0200

    Added new item to the Budget Controller

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // BUDGET CONTROLLER 
 var budgetController = (function() {
 
-	// Create Expense and Income function constructors
-
+	// Create Expense function constructor
 	var Expense = function(id, description, value) {
 		this.id = id;
 		this.description = description;
 		this.value = value;
 	};
 
-	var Income = function() {
+	// Create Income function constructor
+	var Income = function(id, description, value) {
 		this.id = id;
 		this.description = description;
 		this.value = value;
@@ -21,7 +21,6 @@ var budgetController = (function() {
 	  For example if the user will input 15 incomes, we need to create 15 income
 	  objects, and we can store these 15 incomes into an array.
 	*/
-
 	var data = {
 		allItems: {
 			exp: [],
@@ -30,6 +29,39 @@ var budgetController = (function() {
 		totalItems: {
 			exp: 0,
 			inc: 0
+		}
+	};
+
+	return {
+		addItem: function(type, des, val) {
+			var newItem, ID;
+
+			//[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+			// ID = last ID + 1
+
+			// Create new ID
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			} else {
+				ID =0;
+			}
+
+			// Create new item based on 'inc' or 'exp' type
+			if (type === 'exp') {
+				newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			}
+
+			// Push it into our data structure
+			data.allItems[type].push(newItem);
+
+			// Return the new element
+			return newItem;
+		},
+		testing: function() {
+			console.log(data);
 		}
 	};
 
@@ -83,11 +115,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 	}
 
 	var ctrlAddItem = function() {
+		var input, newItem;
 
 		// 1. Get the field input data
-		var input = UICtrl.getInput();
+		input = UICtrl.getInput();
 
 		// 2. Add the item to the budget controller
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		// 3. Add the item to the UI
 
